55772b5b578945fee04e29d2eeb94c8c6f080cc4
Bruno Tavares <connect+github@bltavares.com>
2017-07-16 16:39:45 -0300

    Migra site para o Nickel

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,28 @@
-extern crate iron;
-
-use iron::prelude::*;
-use iron::headers::ContentType;
-use iron::modifiers;
-use iron::status;
+#[macro_use]
+extern crate nickel;
+use nickel::{Nickel, QueryString, HttpRouter};
 
 const INDEX: &str = include_str!("index.html");
 
-fn hello_world(_: &mut Request) -> IronResult<Response> {
-    Ok(Response::with(
-        (status::Ok, modifiers::Header(ContentType::html()), INDEX),
-    ))
-}
-
 fn main() {
+    let mut server = Nickel::new();
+
+    server.get(
+        "/",
+        middleware!{ |request|
+                      let name = request.query()
+                                        .get("name")
+                                        .unwrap_or("TDC");
+
+                      INDEX.replace("TDC", name)
+        },
+    );
+
     let port = std::env::var("PORT").unwrap_or("3000".into());
     let url = format!("0.0.0.0:{port}", port = port);
-
-    Iron::new(hello_world)
-        .http(&url)
-        .expect("Não conseguimos iniciar o servidor");
-
     println!("On {}", url);
+
+    server
+        .listen(url)
+        .expect("Não conseguimos iniciar o servidor");
 }
