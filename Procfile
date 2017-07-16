293d4a5a0604d9d5ad344005e531141b30c0f3ab
Bruno Tavares <connect+github@bltavares.com>
2017-07-16 16:15:05 -0300

    Exemplo do site me Iron

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: target/release/meu-site-em-rust
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate iron;
+
+use iron::prelude::*;
+use iron::headers::ContentType;
+use iron::modifiers;
+use iron::status;
+
+const INDEX: &str = include_str!("index.html");
+
+fn hello_world(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with(
+        (status::Ok, modifiers::Header(ContentType::html()), INDEX),
+    ))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let port = std::env::var("PORT").unwrap_or("3000".into());
+    let url = format!("0.0.0.0:{port}", port = port);
+
+    Iron::new(hello_world)
+        .http(&url)
+        .expect("Não conseguimos iniciar o servidor");
+
+    println!("On {}", url);
 }
