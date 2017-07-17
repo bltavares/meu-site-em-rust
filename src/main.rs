8cb3fc98f772d35f4c0c9ecea8cd19f202b4f23d
Bruno Tavares <connect+github@bltavares.com>
2017-07-16 22:53:22 -0300

    Simplificando o exemplo para caber melhor na apresentacao

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,17 @@
 #[macro_use]
 extern crate nickel;
-use nickel::{Nickel, QueryString, HttpRouter};
+use nickel::{Nickel, HttpRouter};
 
 const INDEX: &str = include_str!("index.html");
 
 fn main() {
     let mut server = Nickel::new();
-
-    server.get(
-        "/",
-        middleware!{ |request|
-                      let name = request.query()
-                                        .get("name")
-                                        .unwrap_or("TDC");
-
-                      INDEX.replace("TDC", name)
-        },
-    );
+    server.get("/", middleware!(INDEX));
 
     let port = std::env::var("PORT").unwrap_or("3000".into());
     let url = format!("0.0.0.0:{port}", port = port);
-    println!("On {}", url);
 
-    server
-        .listen(url)
-        .expect("Não conseguimos iniciar o servidor");
+    println!("On {}", url);
+    server.listen(url)
+          .expect("Não conseguimos iniciar o servidor");
 }
